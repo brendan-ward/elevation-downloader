4db03dc20a42e616ad59df34a26a189ee7198e29
Brendan Ward <bcward@consbio.org>
2019-02-13 12:10:05 -0800

    Migrated to async approach

--- download.py
+++ download.py
@@ -1,5 +1,9 @@
+import asyncio
 from collections import namedtuple
-from requests import Session
+
+from tqdm import tqdm
+from aiohttp import ClientSession, TCPConnector
+
 
 from pymbtiles import MBtiles
 
@@ -10,55 +14,79 @@ EMPTY_TILE = 757  # if content-length is this value, tile is empty and not usefu
 Tile = namedtuple("Tile", ["z", "x", "y"])
 
 
-outfilename = "../data/elevation.mbtiles"
-mode = "w"  # "w"
-zoom = [0, 2]
-skip_existing = True
-
-tiles = []
-for z in range(zoom[0], zoom[1] + 1):
-    for i in range(0, 2 ** z):
-        for j in range(0, 2 ** z):
-            tiles.append(Tile(z, i, j))
+def flip_y(tile):
+    """ Invert tile Y coordinate to match xyz tiling scheme """
+    return Tile(tile.z, tile.x, (1 << tile.z) - 1 - tile.y)
 
 
-# tiles = (Tile(0, 0, 0), Tile(12, 692, 1800))
+@asyncio.coroutine
+def wait_with_progress(futures):
+    for f in tqdm(asyncio.as_completed(futures), total=len(futures)):
+        yield from f
 
-with MBtiles(outfilename, mode) as out:
-    if mode == "w":
-        out.meta = {
-            "name": "elevation",
-            "description": "Mapzen Terrarium Elevation Tiles",
-            "version": "1.0",
-            "attribution": "Mapzen",
-            "credits": "Mapzen",
-            "type": "overlay",
-            "format": "png",
-            # 'bounds': ','.join('{0:4f}'.format(v) for v in bounds),
-            # 'center': [0,0,0],
-            # 'minzoom': str(zoom[0]),
-            # 'maxzoom': str(zoom[-1]),
-        }
 
-    session = Session()
+def download(mbtiles, url, min_zoom, max_zoom, skip_existing=True, concurrency=10):
+    async def fetch_tile(session, url, tile):
+        tile_url = url.format(z=tile.z, x=tile.x, y=tile.y)
 
-    for tile in tiles:
-        if mode == "r+" and skip_existing and out.has_tile(*tile):
-            continue
+        async with session.head(tile_url, ssl=False) as head:
+            length = int(head.headers.get("Content-Length"))
 
-        url = TILE_URL.format(z=tile.z, x=tile.x, y=tile.y)
-        length = int(session.head(url).headers["Content-Length"])
+            if length != EMPTY_TILE:
 
-        if length != EMPTY_TILE:
-            print("downloading {}".format(url))
-            data = session.get(url).content
+                async with session.get(tile_url, ssl=False) as r:
+                    data = await r.read()
+                    # return flip_y(tile), data
+                    mbtiles.write_tile(*flip_y(tile), data=data)
 
-            if data:
-                # Note: invert the Y value since AWS uses a different
-                # tiling scheme
-                y = (1 << tile.z) - 1 - tile.y
-                out.write_tile(z=tile.z, x=tile.x, y=y, data=data)
+            else:
+                print("empty tile: {}".format(tile))
 
-        else:
-            print("empty tile: {}".format(tile))
+    async def fetch_tiles():
+        async with ClientSession(connector=TCPConnector(limit=concurrency)) as session:
+            futures = []
 
+            for z in range(min_zoom, max_zoom + 1):
+                for x in range(0, 2 ** z):
+                    for y in range(0, 2 ** z):
+                        if skip_existing and mbtiles.has_tile(z, x, y):
+                            continue
+
+                        else:
+                            futures.append(
+                                asyncio.ensure_future(
+                                    fetch_tile(session, url, Tile(z, x, y))
+                                )
+                            )
+
+            if len(futures):
+                print("Getting ready to download {} tiles".format(len(futures)))
+
+                for f in tqdm(asyncio.as_completed(futures), total=len(futures)):
+                    await f
+
+            else:
+                print("No tiles to download")
+
+    asyncio.get_event_loop().run_until_complete(fetch_tiles())
+
+
+min_zoom = 0
+max_zoom = 5
+
+with MBtiles("../data/elevation.mbtiles", "r+") as mbtiles:
+    mbtiles.meta = {
+        "name": "elevation",
+        "description": "Mapzen Terrarium Elevation Tiles",
+        "version": "1.0",
+        "attribution": "Mapzen",
+        "credits": "Mapzen",
+        "type": "overlay",
+        "format": "png",
+        "bounds": ",".join(str(x) for x in [-180, -85, 180, 85]),
+        "center": ",".join(str(x) for x in [0, 0, 0]),
+        "minzoom": str(min_zoom),
+        "maxzoom": str(max_zoom),
+    }
+
+    download(mbtiles, TILE_URL, min_zoom, max_zoom, True)
