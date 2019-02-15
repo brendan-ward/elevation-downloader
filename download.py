ed55fa98a29e888f6f14608b00ddfa637ed45071
Brendan Ward <bcward@consbio.org>
2019-02-14 17:00:22 -0800

    Make downloader more resumable with missing tiles

--- download.py
+++ download.py
@@ -1,4 +1,7 @@
+import os
+import glob
 import asyncio
+import pickle
 
 from tqdm import tqdm
 from aiohttp import ClientSession, TCPConnector, ClientTimeout
@@ -7,7 +10,7 @@ from mercantile import tiles as get_tiles
 
 from pymbtiles import MBtiles, Tile
 
-CONCURRENCY = 10
+CONCURRENCY = 30
 BATCH_SIZE = 1000  # number of tiles per batch
 TILE_URL = "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png"
 EMPTY_TILE = 757  # if content-length is this value, tile is empty and not useful
@@ -28,9 +31,7 @@ def flip_y(y, z):
     return (1 << z) - 1 - y
 
 
-def download(
-    mbtiles, url, min_zoom, max_zoom, bounds=None, skip_existing=True, concurrency=10
-):
+def download(mbtiles, url, min_zoom, max_zoom, bounds=None, concurrency=10):
     async def fetch_tile(session, url, z, x, y):
         tile_url = url.format(z=z, x=x, y=y)
 
@@ -65,44 +66,65 @@ def download(
     for zoom in range(min_zoom, max_zoom + 1):
         print("zoom {}".format(zoom))
 
-        tiles = []
+        # read in info about previously read tiles
+        prev_tiles = set()
+        progress_filename = "progress-{}.pickle".format(zoom)
+        if os.path.exists(progress_filename):
+            with open(progress_filename, "rb") as fp:
+                try:
+                    prev_tiles = pickle.load(fp)
+                except:
+                    prev_tiles = set()
+            print("previously read {} tiles".format(len(prev_tiles)))
 
         if bounds is None:
             xy = np.array(
                 np.meshgrid(np.arange(0, 2 ** zoom), np.arange(0, 2 ** zoom))
             ).T.reshape(-1, 2)
 
-            # filter tiles based on ones we already have
-            tiles = [
-                (zoom, x, y)
-                for x, y in xy
-                if not (skip_existing and mbtiles.has_tile(zoom, x, flip_y(y, zoom)))
-            ]
+            tiles = [(zoom, x, y) for x, y in xy]
 
         else:
-            bounded_tiles = get_tiles(*bounds, zooms=[zoom, zoom])
-            tiles = [
-                (z, x, y)
-                for x, y, z in bounded_tiles
-                if not (skip_existing and mbtiles.has_tile(z, x, flip_y(y, z)))
-            ]
+            bounded_tiles = get_tiles(*bounds, zooms=zoom)
+            tiles = [(z, x, y) for x, y, z in bounded_tiles]
+
+        # filter tiles based on ones we already have in the database
+        tiles = [
+            (z, x, y) for z, x, y in tiles if not mbtiles.has_tile(z, x, flip_y(y, z))
+        ]
+
+        if prev_tiles:
+            tiles = [tile for tile in tiles if not tile in prev_tiles]
 
         if tiles:
             print("zoom {} has {} tiles to fetch".format(zoom, len(tiles)))
             for i in range(0, len(tiles), BATCH_SIZE):
-                loop.run_until_complete(fetch_tiles(tiles[i : i + BATCH_SIZE]))
+                tiles_to_fetch = tiles[i : i + BATCH_SIZE]
+                if tiles_to_fetch:
+                    loop.run_until_complete(fetch_tiles(tiles_to_fetch))
+
+                    with open(progress_filename, "wb") as fp:
+                        prev_tiles = prev_tiles | set(tiles_to_fetch)
+                        pickle.dump(prev_tiles, fp)
 
         else:
             print("no tiles to fetch")
 
 
-min_zoom = 0
-max_zoom = 8
+min_zoom = 5
+max_zoom = 5
+mode = "r+"
 
 # Approx bounds of South America
 bounds = [-95.273438, -57.326521, -32.695313, 13.239945]
 
-with MBtiles("../data/elevation.mbtiles", "r+") as mbtiles:  # FIXME: w => r+
+# clear out any progress files
+if mode == "w":
+    for filename in glob.glob("progress-*.pickle"):
+        os.remove(filename)
+
+
+with MBtiles("../data/elevation.mbtiles", mode) as mbtiles:  # FIXME: w => r+
     mbtiles.meta = {
         "name": "elevation",
         "description": "Mapzen Terrarium Elevation Tiles",
